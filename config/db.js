f7d31f1a06f891a3a5b1b275ff878bdce140ac59
zongxili <zongxiliwork@gmail.com>
2021-05-14 19:38:22 -0700

    created database

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const db = config.get('mongoURI'); // get any value from the JSON file
+
+// mongoose.connect(dbs)
+
+const connectDB = async () => { // when async, we should use try/catch
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
+const connectDB = require('./config/db');
+
 const app = express();
+
+// connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
