d1e0ec01b558dc81bc3c69a7b10b0c0dcca1e7f8
zongxili <zongxiliwork@gmail.com>
2021-05-15 23:05:18 -0700

    added routes for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,67 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
+const { route } = require('./users');
 
-// @route  GET api/profile
-// @desc   Test route
-// @access Public
-router.get('/', (req, res)=> res.send('Profile Route'));
+// @route  GET api/profile/me
+// @desc   Get current User's profile
+// @access Private
+router.get('/me', auth, async(req, res)=> {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+    res.json(profile);
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  Post api/profile/profile
+// @desc   Create/Update a User's profile
+// @access Private
+router.post('/', [
+  auth,
+  check('status', 'Status is required').not().isEmpty(),
+  check('skills', 'Skills is required').not().isEmpty()
+  ], async(req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    // Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if(company) profileFields.company = company;
+    if(website) profileFields.website = website;
+    if(location) profileFields.location = location;
+    if(bio) profileFields.bio = bio;
+    if(status) profileFields.status = status;
+    if(githubusername) profileFields.githubusername = githubusername;
+
+
+});
+
 
 module.exports = router;
