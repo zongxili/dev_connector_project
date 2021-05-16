ad93ff620a0f72d0e204e2e4f353599ab52c70a7
zongxili <zongxiliwork@gmail.com>
2021-05-15 22:08:17 -0700

    added functions that validates users token

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
-const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const User = require('../../models/User'); // import User model
-const { getMaxListeners } = require('../../models/User');
 
 // @route  POST api/users
 // @desc   Register user
@@ -22,7 +22,7 @@ router.post('/', [
     'Please enter a password with 6 or more characters'
   ).isLength({min: 6})
 ], 
-async(req, res)=> {
+async (req, res)=> {
   const errors = validationResult(req);
   if(!errors.isEmpty()){ // only if the response returns errors
     return res.status(400).json({
@@ -35,11 +35,11 @@ async(req, res)=> {
   try {
     // See if user exists
     let user = await User.findOne({
-      email
+      email // search by the email in response
     });
 
     if (user){
-      return res.status(400).json({errors:[{ msg: 'User already exists' }]}); // this looks exactly same
+      return res.status(400).json({errors:[{ msg: 'User already exists' }]}); // this looks exactly same as the error above
     }
 
     // Get users gravatar
@@ -62,16 +62,27 @@ async(req, res)=> {
     user.password = await bcrypt.hash(password, salt);
 
     await user.save();
-    // Return JSON webtoken, once user registers, then he should be logged in right away
 
+    // Return JSON webtoken, once users register, then they should be logged in right away
+    const payload ={
+      user: {
+        id: user.id
+      }
+    };
 
-
-    res.send('User Rigistered'); // if no errors, the reponse should be this text
+    jwt.sign(
+      payload, // must be an object
+      config.get('jwtSecret'),
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token }); // show the token in Postman's response
+      }
+    );
   } catch (err){
     console.error(err.message);
     res.status(500).send('Server error');
   }
-
 });
 
 module.exports = router;
