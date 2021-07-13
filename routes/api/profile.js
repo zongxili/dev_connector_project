71de8a5158eb13b32cf3a8731cc9b0d4f4f49b02
zongxili <zongxiliwork@gmail.com>
2021-07-13 00:46:35 -0700

    added route for removing profile & user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -130,7 +130,26 @@ router.get('/user/:user_id', async (req, res) => {
     }
     res.status(500).send('Server Error');
   }
-  // res.json("adfs");
+});
+
+// @route  DELETE api/profile/
+// @desc   Delete profile, user & posts
+// @access Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove users posts
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: "User removed" });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
