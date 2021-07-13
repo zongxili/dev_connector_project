96535672e421d1e45ae84b6b913ed6a8733eec7a
zongxili <zongxiliwork@gmail.com>
2021-07-13 01:08:03 -0700

    added a route for editing the Experience field in the Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -132,7 +132,7 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
-// @route  DELETE api/profile/
+// @route  DELETE api/profile
 // @desc   Delete profile, user & posts
 // @access Private
 router.delete('/', auth, async (req, res) => {
@@ -148,6 +148,55 @@ router.delete('/', auth, async (req, res) => {
     if (err.kind == 'ObjectId') {
       return res.status(400).json({ msg: "Profile not found" });
     }
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  PUT api/profile/experience
+// @desc   Add profile experience
+// @access Private
+router.put('/experience', [auth, [
+  check('title', 'Title is required')
+    .not()
+    .isEmpty(),
+  check('company', 'Company is required')
+    .not()
+    .isEmpty(),
+  check('from', 'Start Date is required')
+    .not()
+    .isEmpty()
+]], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {
+    title,
+    company,
+    location,
+    from,
+    to,
+    current,
+    description
+  } = req.body;
+
+  const newExp = {
+    title,
+    company,
+    location,
+    from,
+    to,
+    current,
+    description
+  }
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    profile.experience.unshift(newExp); // push to the beginning
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.err(err.message);
     res.status(500).send('Server Error');
   }
 });
