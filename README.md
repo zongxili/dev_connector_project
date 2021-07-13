a4966dbe148326c02078eac37bfddd0492fbe97f
zongxili <zongxiliwork@gmail.com>
2021-07-13 00:25:03 -0700

    added routes for Updating/Creating Users and Getting profile by its ID

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # dev_connector_project
 
 type ```npm run server```
+
 Just a reference for myself: https://github.com/bradtraversy/devconnector_2.0
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -121,10 +121,13 @@ router.get('/user/:user_id', async (req, res) => {
   try {
     const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
     if (!profile)
-      return res.status(400).json({ msg: "There is no profile for this user" });
+      return res.status(400).json({ msg: "Profile not found" });
     res.json(profile);
   } catch (err) {
     console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
     res.status(500).send('Server Error');
   }
   // res.json("adfs");
