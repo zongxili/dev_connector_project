ffec4ace3d28be76cb21712b7fa2cff228f83f7f
zongxili <zongxiliwork@gmail.com>
2021-07-13 01:19:11 -0700

    added a route for deleting experience in profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -196,7 +196,26 @@ router.put('/experience', [auth, [
     await profile.save();
     res.json(profile);
   } catch (err) {
-    console.err(err.message);
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  DELETE api/profile/experience/:exp_id
+// @desc   Delete experience from profile
+// @access Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+    profile.experience.splice(removeIndex, 1);
+    // resaving it
+    await profile.save();
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
     res.status(500).send('Server Error');
   }
 });
