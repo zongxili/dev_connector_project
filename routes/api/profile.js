d5f53555472c1fa445a66f91637f52dadc4bfea9
zongxili <zongxiliwork@gmail.com>
2021-07-17 23:14:01 -0700

    added work

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -294,9 +294,20 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 router.get('/github/:username', async (req, res) => {
   try {
     const options = {
-      // @todo
-      uri: `https://api.github.com/users/${req.params.username}/repos?pre_page=`
-    }
+      // 5 per page & sort by created date
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+      if (response.stateCode !== 200) {
+        res.status(404).json({ msg: 'No Github profile found' });
+      }
+      // if it is found
+      res.json(JSON.parse(body)); // without JSON.parse, it will be a regular string with escaped code
+    });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
