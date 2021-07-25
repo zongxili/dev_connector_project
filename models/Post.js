90e53bd6b10aa1117a9ba3f3fe657e85c01d8644
zongxili <zongxiliwork@gmail.com>
2021-07-24 17:07:01 -0700

    added Post.js

---
+++ models/Post.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users' // so that users can only delete their own post, and also shows which user created the post
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  avatar: {
+    // name of the user
+    // so if a user deletes her/his account, the post can be remained
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users' // then we know which likes came from which user
+        // also, this restricts the users can only like a post once
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users' // then we know which likes came from which user
+        // also, this restricts the users can only like a post once
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: { // the day for the actual post
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Post = mongoose.model('post', Post);
