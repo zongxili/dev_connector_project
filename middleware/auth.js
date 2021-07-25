54f3bb7b2bbe8080bd03c2d88f08efe7e1569ff9
zongxili <zongxiliwork@gmail.com>
2021-07-24 19:05:34 -0700

    added Router for Adding a Post, Getting all posts, Deleting a post

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,15 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
-module.exports = function(req, res, next) { // validate the token | next: the callback to next middleware
+module.exports = function (req, res, next) {
+  // validate the token when the API access is private
+  // next variable: the callback to next middleware
+
   // Get the token from header
   const token = req.header('x-auth-token');
-  
-  // Check if no token exists
-  if (!token){
+
+  // Check if token exists
+  if (!token) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
@@ -15,7 +18,7 @@ module.exports = function(req, res, next) { // validate the token | next: the ca
     const decoded = jwt.verify(token, config.get('jwtSecret'));
     req.user = decoded.user;
     next();
-  } catch(err) {
+  } catch (err) {
     res.status(401).json({ msg: 'Token is not valid' });
   }
 };
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -40,4 +40,66 @@ router.post('/', [auth, [
   }
 });
 
+// @route  GET api/posts
+// @desc   Get all posts
+// @access Private (because we can't see the Post page unless logging in)
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 }); // find the most recent first
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  GET api/posts/:id
+// @desc   Get post by User ID
+// @access Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    // if the ID is not valid
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found & given ID is invalid' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  DELETE api/posts/:id
+// @desc   Delete a post by its ID
+// @access Private (because we can't see the Post page unless logging in)
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // if the post doesn't exist
+    if (!post) {
+      return res.status(404).json({ msg: 'Post doesn\' exist or Post not found' });
+    }
+
+    // making sure the User who wants to delete the post owns this post
+    if (post.user.toString() !== req.user.id) { // the first one will be an object ID
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+    await post.remove();
+    res.json({ msg: 'Post removed' });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found & given ID is invalid' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
