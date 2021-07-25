a3c7e50421f556de8d4084cf08348e95909baf49
zongxili <zongxiliwork@gmail.com>
2021-07-24 18:08:19 -0700

    added "Add Post" Route

--- models/Post.js
+++ models/Post.js
@@ -56,4 +56,4 @@ const PostSchema = new Schema({
   }
 });
 
-module.exports = Post = mongoose.model('post', Post);
+module.exports = Post = mongoose.model('post', PostSchema);
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const { ValidationHalt } = require('express-validator/src/base');
+const auth = require('../../middleware/auth');
+const Post = require('../../models/Post');
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+// const { route } = require('./users');
+// const request = require('request');
+// const config = require('config');
 
-// @route  GET api/posts
-// @desc   Test route
-// @access Public
-router.get('/', (req, res)=> res.send('Posts Route'));
+// @route  POST api/posts
+// @desc   Create a post
+// @access Private
+router.post('/', [auth, [
+  check('text', 'Text is required').not().isEmpty()]
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const user = await User.findById(req.user.id).select('-password'); // not send the password back
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    });
+
+    const post = await newPost.save();
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
