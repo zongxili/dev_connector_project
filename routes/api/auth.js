18f4deaeac2f832e99a355fc953d6d6a3cf90f8b
zongxili <zongxiliwork@gmail.com>
2021-07-27 23:16:06 -0700

    changed the styling

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -10,7 +10,7 @@ const config = require('config');
 // @route  GET api/auth
 // @desc   Test route
 // @access Public
-router.get('/', auth, async(req, res)=>{
+router.get('/', auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select('-password'); // remove the password from token
     res.json(user);
@@ -31,52 +31,52 @@ router.post('/', [
     'password',
     'Password is required'
   ).exists() // in this case, emypt field is fine
-], 
-async (req, res)=> {
-  const errors = validationResult(req);
-  if(!errors.isEmpty()){ // only if the response returns errors
-    return res.status(400).json({
-      errors: errors.array()
-    });
-  }
-
-  const { email, password } = req.body; // get from the request
-
-  try {
-    // See if user exists
-    let user = await User.findOne({
-      email // search by the email in response
-    });
-
-    if (!user){
-      return res.status(400).json({errors:[{ msg: 'Invalid Credentials' }]});
+],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) { // only if the response returns errors
+      return res.status(400).json({
+        errors: errors.array()
+      });
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const { email, password } = req.body; // get from the request
 
-    if (!isMatch) {
-      return res.status(400).json({errors:[{ msg: 'Invalid Credentials' }]}); // Post the same message as above so user doesn't know if the account exists
+    try {
+      // See if user exists
+      let user = await User.findOne({
+        email // search by the email in response
+      });
+
+      if (!user) {
+        return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] }); // Post the same message as above so user doesn't know if the account exists
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload, // must be an object
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token }); // show the token in Postman's response
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
     }
-
-    const payload ={
-      user: {
-        id: user.id
-      }
-    };
-
-    jwt.sign(
-      payload, // must be an object
-      config.get('jwtSecret'),
-      { expiresIn: 360000 },
-      (err, token) => {
-        if (err) throw err;
-        res.json({ token }); // show the token in Postman's response
-      }
-    );
-  } catch (err){
-    console.error(err.message);
-    res.status(500).send('Server error');
-  }
-});
+  });
 
 module.exports = router;
