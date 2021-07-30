4b1ddee687b637455929b35845c68a89cd8851b9
zongxili <zongxiliwork@gmail.com>
2021-07-30 10:50:31 -0700

    added Navbar and Landing components along with the images

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
-
+import Navbar from './components/layout/Navbar';
+import { Landing } from './components/layout/Landing';
 import './App.css';
 
 const App = () =>
   <Fragment>
-    <h1>App</h1>
+    <Navbar />
+    <Landing />
   </Fragment>
 
 export default App;
