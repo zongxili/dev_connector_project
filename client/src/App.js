9505bb4b7ee87cd9eb045b4bf4ac0247a5dd8354
zongxili <zongxiliwork@gmail.com>
2021-07-30 16:59:25 -0700

    created and added Rigister & Login components

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React, { Fragment, } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
+import { Login } from './components/auth/Login';
+import { Register } from './components/auth/Register';
+
 import './App.css';
 
 const App = () => (
@@ -9,8 +12,15 @@ const App = () => (
     <Fragment>
       <Navbar />
       <Route exact path='/' component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
       <Landing />
     </Fragment>
   </ Router >
 );
+
 export default App;
