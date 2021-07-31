6fceed5ca5f59540e401f86eef046a6e5195d215
zongxili <zongxiliwork@gmail.com>
2021-07-30 22:11:36 -0700

    added form and set onChange function

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 
 export const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const onCharge = e => setFormData({ ...formData, [e.target.name]: e.target.value }); // by doing this, the target will be as same as the name
+
+  const { name, email, password, password2 } = formData;
+
   return (
-    <div>
-      Register
-    </div>
+    <Fragment>
+      <h1 className='large text-primary'>Sign Up</h1>
+      <p className='lead'><i className='fas fa-user'></i> Create Your Account</p>
+      <form className='form' action='create-profile.html'>
+        <div className='form-group'>
+          <input type='text'
+            placeholder='Name'
+            name='name'
+            value={name}
+            onChange={e => onCharge(e)}
+            required />
+        </div>
+        <div className='form-group'>
+          <input type='email' placeholder='Email Address' name='email' />
+          <small className='form-text'
+          >This site uses Gravatar so if you want a profile image, use a
+            Gravatar email</small
+          >
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            minLength='6'
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Confirm Password'
+            name='password2'
+            minLength='6'
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Register' />
+      </form>
+      <p className='my-1'>
+        Already have an account? <a href='login.html'>Sign In</a>
+      </p>
+    </Fragment>
   )
 }
