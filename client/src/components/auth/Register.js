c2d7218ac806cca4177e0edee8801672789ea47f
zongxili <zongxiliwork@gmail.com>
2021-07-30 22:19:55 -0700

    built validation of the form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -12,25 +12,41 @@ export const Register = () => {
 
   const { name, email, password, password2 } = formData;
 
+  const onSubmit = e => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("Passwords do not match");
+    } else {
+      console.log(formData);
+    }
+  }
+
   return (
     <Fragment>
       <h1 className='large text-primary'>Sign Up</h1>
       <p className='lead'><i className='fas fa-user'></i> Create Your Account</p>
-      <form className='form' action='create-profile.html'>
+      <form className='form' onSubmit={e => onSubmit(e)}>
         <div className='form-group'>
           <input type='text'
             placeholder='Name'
             name='name'
             value={name}
             onChange={e => onCharge(e)}
-            required />
+            required
+          />
         </div>
         <div className='form-group'>
-          <input type='email' placeholder='Email Address' name='email' />
-          <small className='form-text'
-          >This site uses Gravatar so if you want a profile image, use a
-            Gravatar email</small
-          >
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={e => onCharge(e)}
+            required
+          />
+          <small className='form-text'>
+            This site uses Gravatar so if you want a profile image, use a Gravatar email
+          </small>
         </div>
         <div className='form-group'>
           <input
@@ -38,6 +54,9 @@ export const Register = () => {
             placeholder='Password'
             name='password'
             minLength='6'
+            value={password}
+            onChange={e => onCharge(e)}
+            required
           />
         </div>
         <div className='form-group'>
@@ -46,9 +65,16 @@ export const Register = () => {
             placeholder='Confirm Password'
             name='password2'
             minLength='6'
+            value={password2}
+            onChange={e => onCharge(e)}
+            required
           />
         </div>
-        <input type='submit' className='btn btn-primary' value='Register' />
+        <input
+          type='submit'
+          className='btn btn-primary'
+          value='Register'
+        />
       </form>
       <p className='my-1'>
         Already have an account? <a href='login.html'>Sign In</a>
