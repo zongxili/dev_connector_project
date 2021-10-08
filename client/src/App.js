f5a04b70095fb12ddb463a3d1eca51d7248c3798
zongxili <zongxiliwork@gmail.com>
2021-10-07 23:47:55 -0700

    added more notes

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 import { Provider } from 'react-redux'; // Redux
 import store from './store';
 import { loadUser } from './actions/auth';
@@ -38,7 +39,7 @@ const App = () => {
               route can go to different paths but same component 
             */}
               <Route exact path="/login" component={Login} />
-              <Route exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
