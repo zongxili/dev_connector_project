ab2bc5c1f5b6197e16cf6b78ecc5ad8a17eca52c
zongxili <zongxiliwork@gmail.com>
2021-10-30 09:12:02 -0700

    added routes

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,6 +2,7 @@
 import { combineReducers } from "redux";
 import alert from './alert';
 import auth from './auth';
+import profile from './profile';
 
 export default combineReducers({
   // this variable will be showed up in the Inspect Redux Plugin 
