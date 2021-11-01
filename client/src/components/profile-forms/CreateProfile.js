c4efaae1ad8c69060c12f5503893ac9f04a6fb75
zongxili <zongxiliwork@gmail.com>
2021-11-01 16:41:56 -0700

    created Submit Form action

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { useState, Fragment } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { createProfile } from '../../actions/profile';
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
   const [formData, setFormData] = useState({
     company: '',
     website: '',
@@ -37,6 +39,12 @@ const CreateProfile = props => {
 
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
+
+  const onSubmit = e => {
+    e.preventDefault();
+    createProfile(formData, history);
+  };
+
   return (
     <Fragment>
       <h1 className="large text-primary">
@@ -47,7 +55,7 @@ const CreateProfile = props => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={e => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -152,7 +160,7 @@ const CreateProfile = props => {
 };
 
 CreateProfile.propTypes = {
-
+  createProfile: PropTypes.func.isRequired,
 };
 
-export default CreateProfile;
+export default connect(null, { createProfile })(withRouter(CreateProfile));
