494e8928dd31782848aca6db7cd80de63c46bc77
zongxili <zongxiliwork@gmail.com>
2021-11-03 23:58:41 -0700

    added more notes

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -24,6 +24,7 @@ export const getCurrentProfile = () => async dispatch => {
   }
 };
 
+
 // Create or update profile
 // @history: redirect to the Client-Side Route after users submit the form
 export const createProfile = (formData, history, edit = false) => async dispatch => {
