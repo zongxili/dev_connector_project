a4caae4a23b562cb57eaf3b844badb0de7b4a385
zongxili <zongxiliwork@gmail.com>
2021-11-06 23:51:20 -0700

    fixed the typo & added more dependencies in useEffect

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -23,23 +23,26 @@ const EditProfile = ({ profile: { profile, loading }, createProfile, getCurrentP
   const [displaySocialInputs, toggleSocialInputs] = useState(false);
 
   useEffect(() => {
-    getCurrentProfile();
+    if (!profile) getCurrentProfile();
 
-    setFormData({
-      company: loading || !profile.company ? '' : profile.company,
-      website: loading || !profile.website ? '' : profile.website,
-      location: loading || !profile.location ? '' : profile.location,
-      status: loading || !profile.status ? '' : profile.status,
-      skills: loading || !profile.skills ? '' : profile.skills.join(','),
-      githubusername: loading || !profile.githubusername ? '' : profile.githubusername,
-      bio: loading || !profile.bio ? '' : profile.bio,
-      twitter: loading || !profile.twitter ? '' : profile.social.twitter,
-      facebook: loading || !profile.facebook ? '' : profile.social.facebook,
-      linkedin: loading || !profile.linkedin ? '' : profile.social.linkedin,
-      youtube: loading || !profile.youtube ? '' : profile.social.youtube,
-      instagram: loading || !profile.instagram ? '' : profile.social.instagram
-    })
-  }, [loading])
+    if (!loading && profile) {
+      setFormData({
+        company: loading || !profile.company ? "" : profile.company,
+        website: loading || !profile.website ? "" : profile.website,
+        location: loading || !profile.location ? "" : profile.location,
+        status: loading || !profile.status ? "" : profile.status,
+        skills: loading || !profile.skills ? "" : profile.skills,
+        githubusername:
+          loading || !profile.githubusername ? "" : profile.githubusername,
+        bio: loading || !profile.bio ? "" : profile.bio,
+        twitter: loading || !profile.social ? "" : profile.social.twitter,
+        facebook: loading || !profile.social ? "" : profile.social.facebook,
+        linkedin: loading || !profile.social ? "" : profile.social.linkedin,
+        youtube: loading || !profile.social ? "" : profile.social.youtube,
+        instagram: loading || !profile.social ? "" : profile.social.instagram,
+      });
+    }
+  }, [loading, getCurrentProfile, profile]);
 
   const {
     company,
@@ -183,11 +186,11 @@ EditProfile.propTypes = {
   profile: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state = ({
+const mapStateToProps = state => ({
   profile: state.profile
 });
 
 export default connect(
   mapStateToProps,
-  null, { createProfile, getCurrentProfile }
+  { createProfile, getCurrentProfile }
 )(withRouter(EditProfile));
