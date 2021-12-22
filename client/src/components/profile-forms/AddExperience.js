c3c2390f9627f9d3ca00edbfda290cf8e4a44dc6
zongxili <zongxiliwork@gmail.com>
2021-12-21 23:08:20 -0800

    Formated the code

--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -63,7 +63,10 @@ const AddExperience = ({ addExperience, history }) => {
         </div>
         <div className="form-group">
           <h4>To Date</h4>
-          <input type="date" name="to" value={to}
+          <input
+            type="date"
+            name="to"
+            value={to}
             onChange={e => onChange(e)}
             disabled={toDateDisabled ? 'disabled' : ''}
           />
