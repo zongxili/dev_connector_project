4c7bce87023b9df6d6434ee1776aad369854bf4b
zongxili <zongxiliwork@gmail.com>
2021-12-31 08:10:27 -0800

    added the Profile component but still having the Spinner issue

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 import PrivateRoute from './components/routing/PrivateRoute';
 import { Provider } from 'react-redux'; // Redux
 import store from './store';
@@ -45,6 +46,7 @@ const App = () => {
             */}
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:id" component={Profile} />
               <PrivateRoute exact
                 path="/dashboard"
                 component={Dashboard}
