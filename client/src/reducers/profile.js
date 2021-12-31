92ebb63cbd62c857c141b208dce39698ac66b86d
zongxili <zongxiliwork@gmail.com>
2021-12-31 06:46:48 -0800

    added the Action and Reducer for Get all Profile & Github Repos

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,8 +1,15 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from "../actions/types";
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE,
+  GET_PROFILES,
+  GET_REPOS
+} from "../actions/types";
 
 const initialState = {
   profile: null,
-  prfiles: [],
+  profiles: [],
   repos: [],
   loading: true,
   error: {}
@@ -19,6 +26,12 @@ export default function (state = initialState, action) {
         profile: payload,
         loading: false
       };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false
+      };
     case PROFILE_ERROR:
       return {
         ...state,
@@ -26,6 +39,13 @@ export default function (state = initialState, action) {
         loading: false
       };
     case CLEAR_PROFILE:
+      return {
+        ...state,
+        profile: null,
+        repos: [],
+        loading: false
+      };
+    case GET_REPOS:
       return {
         ...state,
         profile: null,
