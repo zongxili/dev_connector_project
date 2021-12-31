ad68f9a5b9672255103916440b70505fd233297b
zongxili <zongxiliwork@gmail.com>
2021-12-31 07:22:00 -0800

    completed the individual Profiles component

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -1,17 +1,40 @@
 // Child Component of ./Profiles
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const ProfileItem = props => {
+const ProfileItem = ({ profile: {
+  user: { _id, name, avatar },
+  status,
+  company,
+  location,
+  skills
+} }) => {
   return (
-    <div>
-
+    <div className='profile bg-light'>
+      <img src={avatar} alt='' className='round-img'></img>
+      <div>
+        <h2>{name}</h2>
+        <p>{status} {company && <span> at {company}</span>}</p>
+        <p className='my-1'>{location && <span>{location}</span>}</p>
+        <Link to={`/profile/${_id}`} className='btn btn-primary'>
+          View Profile
+        </Link>
+      </div>
+      <ul>
+        {skills.slice(0, 4).map((
+          skill, index) => (
+          <li key={index} className='text-primary'>
+            <i className='fas fa-check'>{skills}</i>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 };
 
 ProfileItem.propTypes = {
-
+  profile: PropTypes.object.isRequired,
 };
 
 export default ProfileItem;
