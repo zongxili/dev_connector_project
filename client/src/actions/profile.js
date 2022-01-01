396651dc173d6344bb94efb01f99d552a9be0fdd
zongxili <zongxiliwork@gmail.com>
2022-01-01 15:15:53 -0800

    fixed the Spinner loading error

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -54,7 +54,7 @@ export const getProfileById = userId => async dispatch => {
   try {
     const res = await axios.get(`/api/profile/user/${userId}`);
     dispatch({
-      type: GET_PROFILES,
+      type: GET_PROFILE,
       payload: res.data
     });
   } catch (error) {
